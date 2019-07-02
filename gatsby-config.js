92530f5746e06618df3682a6f72e2cdaba365d44
Frank Hernandez <fhern077@fiu.edu>
2019-07-01 22:30:29 -0400

    added netlify cms as a dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
     "gatsby-plugin-typescript",
     "gatsby-plugin-tslint",
     "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
